dd0350f48685284e4e3ce8fa768b68cdc3bd789e
along45 <142496665+along45@users.noreply.github.com>
2023-12-04 16:06:06 +0200

    Update API_Key_Go.go

--- API_Key_Go.go
+++ API_Key_Go.go
@@ -1,5 +1,6 @@
 func main() {
     apiKey := "1234567"
+    key :="JuDmGuXNc5ZghYnpMNn+oajg1xZu0ddNA57QwE2i0AaLcMhTRQISzZD3mUyPNQNe1TQPW7fM5xIFNDOU7sPHFA=="
 
     // Make an API call using the hardcoded API key
     response, err := http.Get("https://api.example.com/v1/data?apiKey=" + apiKey)
